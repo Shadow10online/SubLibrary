647d26bfbffe3194233d1b4c8758fe2c91842474
shadowliu1202 <shadow10online@gmail.com>
2017-12-03 12:20:36 +0800

    test commit 2

---
+++ src/test/java/com/shadow/testsubmodule/MainTestTest.java
@@ -0,0 +1,16 @@
+package com.shadow.testsubmodule;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by 劉志鴻 on 2017/12/3.
+ */
+public class MainTestTest {
+    @Test
+    public void getWTF() throws Exception {
+
+    }
+
+}
