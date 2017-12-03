c7360dc2dd732b051c2a27d528c1ee2229afd814
shadowliu1202 <shadow10online@gmail.com>
2017-12-03 11:51:36 +0800

    test

---
+++ src/main/java/com/shadow/testsubmodule/MainTest.java
@@ -0,0 +1,11 @@
+package com.shadow.testsubmodule;
+
+/**
+ * Created by 劉志鴻 on 2017/12/3.
+ */
+
+public class MainTest {
+    public static String getWTF(){
+        return "Hello World";
+    }
+}
